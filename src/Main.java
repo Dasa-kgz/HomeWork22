a077a0b266e2d6edb4c42788eb1c15f57ee5bee8
AkmatovDastanbek <124769056+AkmatovDastanbek@users.noreply.github.com>
2023-03-05 21:46:17 +0600

    Ready

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+    String separator=File.separator;
+    String path=separator+"E:"+separator+"Сабак Peaksoft"+separator+"Lessons"+separator+"HomeWork22"+separator+"src"+separator+"song.txt";
+
+    File file=new File(path);
+
+    Scanner sc=new Scanner(file);
+    while (sc.hasNextLine()){
+        System.out.println(sc.nextLine());
+    }
+    sc.close();
+
     }
 }
--- src/Song.java
+++ src/Song.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Song {
+public class Song {
+    private String title;
+    private String author;
+    private String lyrics;
+
+    public Song(String title, String author, String lyrics) {
+        this.title = title;
+        this.author = author;
+        this.lyrics = lyrics;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getLyrics() {
+        return lyrics;
+    }
 }
